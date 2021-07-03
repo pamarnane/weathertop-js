f044e41b0569d47b3f698bf0718a6532d9726f0c
Glitch (smiling-organized-brass) <none>
2021-07-03 08:03:55 +0000

    🚤🐥 Checkpoint ./views/partials/menu.hbs:32669004/27 ./views/about.hbs:32669004/98 ./controllers/about.js:32669004/50 ./views/partials/listsongs.hbs:32669004/919 ./views/dashboard.hbs:32669004/92

--- controllers/about.js
+++ controllers/about.js
@@ -6,7 +6,7 @@ const about = {
   index(request, response) {
     logger.info("about rendering");
     const viewData = {
-      title: "About Template 1",
+      title: "Playlist 1 Dashboard",
     };
     response.render("about", viewData);
   },
