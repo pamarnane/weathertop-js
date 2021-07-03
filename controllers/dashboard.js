caed080ccfcc467077eaa50ab2dec6580d59849a
Glitch (smiling-organized-brass) <none>
2021-07-03 08:15:56 +0000

    🐽☕️ Checkpoint ./views/partials/listsongs.hbs:32669004/1598 ./controllers/dashboard.js:32669004/808 ./models/playlist-store.js:32669004/1500

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const logger = require("../utils/logger");
+const sonatas = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Template 1 Dashboard",
+      playlist: sonatas
     };
+    logger.info('about to render', sonatas)
     response.render("dashboard", viewData);
   },
 };
